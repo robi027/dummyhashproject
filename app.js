437f65e167bbdaeb1f00464c6f80357af22165b0
robi027 <robi_dwisetiawan@yahoo.com>
2018-08-13 13:09:46 +0700

    v1.0.2

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const server = require("./server");
+// const server = require("./server");
 const app = express();
 
-app.use(express.static(__dirname + '/views'));
+// app.use(express.static(__dirname + '/views'));
 
-server(app);
+// server(app);
+
+var port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen (port, ()=> console.log(`server running ${port}`));
