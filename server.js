d74a8f7c89e9e7c5ee2a745e2e5d6e2ea4c0ff7b
robi027 <robi_dwisetiawan@yahoo.com>
2018-08-13 12:51:12 +0700

    v1.0.1

---
+++ server.js
@@ -0,0 +1,13 @@
+const port = 3000
+
+module.exports = function(app){
+    
+    app.listen(port, (err) => {
+
+        if (err) {
+          return console.log('something bad happened', err)
+        }
+        
+        console.log(`server is listening on ${port} bro`)
+      })
+}
