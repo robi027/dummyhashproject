e0761ebd46ba9a0b10b44ea2dd8a1b545dc8d23c
robi027 <robi_dwisetiawan@yahoo.com>
2018-08-13 15:36:52 +0700

    server <=> app

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const apps = require("./app");
 const app = express();
 
-app.use(express.static(__dirname + '/views'));
+app.use('', apps);
 
 var port = process.env.PORT || 3000;
 
